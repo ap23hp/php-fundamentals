14b989225d80e23d6674349c523e280d8c53976f
ap23hp <aprakhanna5@gmail.com>
2026-02-06 18:53:01 +0530

    associative arrays

--- index.php
+++ index.php
@@ -20,4 +20,45 @@ foreach($skills as $skill){
 }
 echo "</ul>";
 
+$user=[
+    "name"=> "Apra",
+    "role"=>"php developer",
+    "year"=> 2026
+];
+
+foreach( $user as $key=> $value){
+    echo "$key : $value<br>";
+}
+
+$userObject=new stdClass();
+$userObject->name="abc";
+$userObject->learning = "PHP & WordPress";
+$userObject->year = 2026;
+
+foreach($userObject as $key=>$value){
+    echo  "<p> $key : $value<br></p>";
+}
+
+
+class User {
+    public $namee;
+    public $learning;
+    public $year;
+}
+
+$user1 = new User();
+$user1->namee = "Apra";
+$user1->learning = "PHP & WordPress";
+$user1->year = 2026;
+
+$user2 = new User();
+$user2->namee = "Tina";
+$user2->learning = "PHP";
+$user2->year = 2025;
+
+echo $user1->namee . "<br>";
+echo $user2->namee;
+
+
+
 ?>
