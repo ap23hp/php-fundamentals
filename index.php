06ad703cb7cff906bdd8b9909a4f96cc7517b750
ap23hp <aprakhanna5@gmail.com>
2026-02-06 10:52:27 +0530

    array loop

--- index.php
+++ index.php
@@ -4,6 +4,20 @@ $name="Apra";
 $learning="I am learning php";
 $year=2026;
 echo "Hi, My name is $name and $learning in $year.";
+$skills = ["PHP", "WordPress", "Git", "JavaScript"];
 
+echo "My skills are:<br>";
+
+foreach ($skills as $skill) {
+    echo "- $skill<br>";
+}
+
+echo "<h2>Using h2 heading tag</h2>";
+  echo "<ul>";
+foreach($skills as $skill){
+  
+    echo "<li> $skill</li>";
+}
+echo "</ul>";
 
 ?>
